bb83d44555f0da48d0ed303ea19958f80f0f43f3
Sodiq Aina <Sodiqaina@ymail.com>
2025-05-19 10:38:41 +0100

    Refactor: Move config to environment variables

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKXbOVOj2T3RpCTesbbB2xTSXQuoZBuXs",
-  authDomain: "midel-solutions.firebaseapp.com",
-  projectId: "midel-solutions",
-  storageBucket: "midel-solutions.firebasestorage.app",
-  messagingSenderId: "564034901205",
-  appId: "1:564034901205:web:4914e03e66df8ee6f8fe5c",
-  measurementId: "G-JP8TW2NX5H",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
